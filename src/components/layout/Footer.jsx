a2076be1d32d7d9b6c55261e84114a44cb67027a
Lucas Passos <lucas.depassos@yahoo.com.br>
2021-01-27 14:08:07 -0300

    last commit

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div className='footer has-background-grey is-size-5 has-text-centered has-text-white-bis is-family-secondary'>
-               Lucas Passos - Candidato a Estagiário
+               Lucas Passos 
             </div>
         )
     }
